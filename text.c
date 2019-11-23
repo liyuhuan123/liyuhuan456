0f64d0f7a72d58476a13361cd3ecde8f363b2fe2
liyuhuan123 <2502268134@qq.com>
2019-11-23 19:19:34 +0800

    提交代码

--- text.c
+++ text.c
@@ -1,47 +1,66 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
+void pattern(int star)
+{
+	for (int i = 0; i <= star; i++)//打印上半三角
+	{
+
+		for (int j = 0; j < i * 2 + 1; j++)
+			printf("*");
+	}
+	printf("\n");
+	for (int n = star - 1; n >= 0; n--)//打印下半三角
+	{
+		for (int m = 0; m <= n * 2 - 1; m++)
+			printf("*");
+	}printf("\n");
+}
+/*int main()
+{
+	int x;
+	scanf("%d", &x);
+	pattern(x);
+	return 0;
+	system("pause");
+}*/
+void flower(int num)
+{
+	//水仙花数
+	int unit = num % 10;
+	int ten = num % 100 / 10;
+	int hundered = num / 100;
+	if (unit*unit*unit + ten*ten*ten + hundered*hundered*hundered == num)
+		printf("%d\t", num);
+}
+/*int main()
+{
+	int k;
+	for (k = 100; k < 1000; k++)
+	{
+		flower(k);
+	}
+	return 0;
+	system("pause");
+
+}*/
+void sn(int num)
+{
+	int sum = 0;
+	for (int i = 1; i <= 5; i++)
+	{
+		sum += num;
+		num = num * 10 + num;
+	}
+	printf("%d\n", sum);
+
+}
 int main()
 {
-	/*int n, i=0, j, sum = 0;
-	scanf("%d\n", &n);
-	while (i < n)
-	{
-		scanf("%d", &i);
-		i++;
+		int x;
+		scanf("%d", &x);
+		sn(x);
+		return 0;
+		system("pause");
 	}
-	for (j = 0; j < n; j++)
-	{
-		if (i >= 0)
-			sum += i;
-		else
-			continue;
-	}
-	printf("%d", sum);*/
-	/*int a, b;
-	scanf("%d", &b);
-	a = 2;
-	while (a<b)
-	{
-		if (b%a == 0)
-			break;
-		a++;
-	}
-	if (a == b)
-		printf("yes\n", b);
-	else
-		printf("no\n", b);*/
-	int N;
-	int i;
-	int num = 0;
-	scanf("%d", &N);
-	printf("%d\n", N);
-	for (i = 1; i <= N; i++)
-	{
-		num += (i % 2 == 0) + (i % 3 == 0) + (i % 5 == 0) - (i % 2 == 0) && (i % 5 == 0) && (i % 3 == 0) - (i % 2 == 0) && (i % 5 == 0) - (i % 3 == 0) && (i % 5 == 0) - (i % 2 == 0) && (i % 3 == 0);
 
-	}
-	system("pause");
-	return 0;
-}
