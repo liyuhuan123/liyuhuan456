b3729072a1786649220e7bbfa7d9cd046f9f2c99
liyuhuan123 <2502268134@qq.com>
2019-11-24 13:05:50 +0800

    提交代码

--- text.c
+++ text.c
@@ -1,66 +1,51 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-void pattern(int star)
+int main( )
 {
-	for (int i = 0; i <= star; i++)//打印上半三角
+	/*int a = 1; int b = 2; int t;
+	t = a;
+	a = b;
+	b = t;
+	printf("a=%d,b=%d\n", a, b);*/
+	/*int arr[10] = { 1, 2, 4, 3, 7, 5, 6, 8, 9, 0 };
+	int i,max;
+	max = arr[0];
+	for (i = 0; i < 10; i++)
 	{
+		if (max < arr[i])
+		{
+			max = arr[i];
+		}
 
-		for (int j = 0; j < i * 2 + 1; j++)
-			printf("*");
 	}
-	printf("\n");
-	for (int n = star - 1; n >= 0; n--)//打印下半三角
+	printf("max=%d\n", max);*/
+	/*int a=2, b=3, c=1, t;
+	if (a < b)
 	{
-		for (int m = 0; m <= n * 2 - 1; m++)
-			printf("*");
-	}printf("\n");
-}
-/*int main()
-{
-	int x;
-	scanf("%d", &x);
-	pattern(x);
-	return 0;
+		t = a;
+		a = b;
+		b = t;
+	}
+	if (a < c)
+	{
+		t = a;
+		a = c;
+		c = t;
+	}
+	if (b < c)
+	{
+		t = b;
+		b = c;
+		c = t;
+	}
+	printf("%d,%d,%d\n", a, b, c);*/
+	int a = 23, b = 69, i,j;
+	for (i = 1; i <= a&&i <= b; ++i)
+	{
+		if (a%i == 0 && b%i == 0)
+			j = i;
+	}
+	printf("\n最大公约数是:%d\n", j);
 	system("pause");
-}*/
-void flower(int num)
-{
-	//水仙花数
-	int unit = num % 10;
-	int ten = num % 100 / 10;
-	int hundered = num / 100;
-	if (unit*unit*unit + ten*ten*ten + hundered*hundered*hundered == num)
-		printf("%d\t", num);
-}
-/*int main()
-{
-	int k;
-	for (k = 100; k < 1000; k++)
-	{
-		flower(k);
-	}
 	return 0;
-	system("pause");
-
-}*/
-void sn(int num)
-{
-	int sum = 0;
-	for (int i = 1; i <= 5; i++)
-	{
-		sum += num;
-		num = num * 10 + num;
-	}
-	printf("%d\n", sum);
-
 }
-int main()
-{
-		int x;
-		scanf("%d", &x);
-		sn(x);
-		return 0;
-		system("pause");
-	}
-
