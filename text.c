703b70e47f4393901472d541b85fad0df21f471f
liyuhuan123 <2502268134@qq.com>
2019-11-26 22:01:46 +0800

    提交代码

--- text.c
+++ text.c
@@ -1,66 +1,63 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#include<stdio.h.>
 #include<stdlib.h>
-void pattern(int star)
+/*void game()
 {
-	for (int i = 0; i <= star; i++)//打印上半三角
+	int tuGuess = rand() % 100 + 1;
+	int input = 0;
+	while (1)
 	{
+		printf("请输入猜的数字\n");
+		scanf("%d", &input);
+		if (input > tuGuess)
+		{
+			printf("猜大了\n");
+		}
+		else if (input < tuGuess)
+		{
+			printf("猜小了\n");
+		}
+		else
+		{
+			printf("恭喜你猜对了！");
+			break;
 
-		for (int j = 0; j < i * 2 + 1; j++)
-			printf("*");
+		}
 	}
-	printf("\n");
-	for (int n = star - 1; n >= 0; n--)//打印下半三角
-	{
-		for (int m = 0; m <= n * 2 - 1; m++)
-			printf("*");
-	}printf("\n");
-}
-/*int main()
-{
-	int x;
-	scanf("%d", &x);
-	pattern(x);
-	return 0;
-	system("pause");
-}*/
-void flower(int num)
-{
-	//水仙花数
-	int unit = num % 10;
-	int ten = num % 100 / 10;
-	int hundered = num / 100;
-	if (unit*unit*unit + ten*ten*ten + hundered*hundered*hundered == num)
-		printf("%d\t", num);
-}
-/*int main()
-{
-	int k;
-	for (k = 100; k < 1000; k++)
-	{
-		flower(k);
-	}
-	return 0;
-	system("pause");
-
-}*/
-void sn(int num)
-{
-	int sum = 0;
-	for (int i = 1; i <= 5; i++)
-	{
-		sum += num;
-		num = num * 10 + num;
-	}
-	printf("%d\n", sum);
-
 }
 int main()
 {
-		int x;
-		scanf("%d", &x);
-		sn(x);
-		return 0;
-		system("pause");
+	int input = 0;
+	while (1)
+	{
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:game();
+			break;
+		case 2:
+			break;
+		default:
+			printf("输入有误\n");
+			break;
+		}
+		}*/
+int mian()
+{
+	char arr1[] = "I LOVE YOU!";
+	char arr2[] = "###########";
+	int left = 0;
+	int right = 10;
+	printf("%s\n", arr2);
+	while(left <= right)
+	{
+		Sleep(1000);
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		left++;
+		right--;
+		printf("%s\n", arr2);
 	}
 
+	system("pause");
+	return 0;
+}
