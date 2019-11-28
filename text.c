d8cec7b61afee99ce72da8fdb182233eb3510fc4
liyuhuan123 <2502268134@qq.com>
2019-11-28 22:20:19 +0800

    选择排序算法练习

--- text.c
+++ text.c
@@ -1,35 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>//冒泡排序算法练习
-void buble_sort(int arr[4], int size)
-{
-	int i = 0;
-	int j = 0;
-	int tmp = 0;
-	for (i = 0; i < size; i++)//第一重循环，找size次最小元素
-	{
-		for (j = 0; j < size; j++)//第二重循环，找到这趟比较中的最小元素
-		{
-			if (arr[i] < arr[j])
-			{
-				tmp = arr[i];//不符合升序排序，就交换元素
-				arr[i] = arr[j];
-				arr[j] = tmp;
-			}
-
-		}
-	}
-}
 int main()
 {
-	int i;
-	int arr[4] = { 6, 9, 4, 5 };
-	int size = sizeof(arr) / sizeof(arr[0]);
-	buble_sort(arr, size);//调用冒泡排序函数
-	for (i = 0; i < 4; i++)//打印排序好的有序数组
+	int i, j, min, tmp, a[10] = { 2, 9, 6, 4, 7, 3, 1, 0, 5, 8 };
+	for (i = 0; i < 9; i++)
 	{
-		printf("%d\t", arr[i]);
-
+		min = i;
+		for (j = i + 1; j < 10; j++)
+		{
+			if (a[min]>a[j])
+				min = j;
+			tmp = a[i];
+			a[i] = a[min];
+			a[min] = tmp;
+		}
+	}
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\t", a[i]);
 	}
 	printf("\n");
 	return 0;
