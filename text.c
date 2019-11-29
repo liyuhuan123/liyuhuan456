52013e24720d84729fc631ea75a9361c3b74ccc3
liyuhuan123 <2502268134@qq.com>
2019-11-29 22:27:03 +0800

    完数算法练习

--- text.c
+++ text.c
@@ -1,25 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
+void wannum(int x)
+{
+	int i = 0;
+	int sum = 0;
+	for (i = 1; i < x; i++)
+	{
+		if (x%i == 0)
+			sum += i;
+	}
+	if (sum == i)
+		printf("%d\t", x);
+}
 int main()
 {
-	int i, j, min, tmp, a[10] = { 2, 9, 6, 4, 7, 3, 1, 0, 5, 8 };
-	for (i = 0; i < 9; i++)
+	int i = 1;
+	for (i = 1; i <= 1000; i++)
 	{
-		min = i;
-		for (j = i + 1; j < 10; j++)
-		{
-			if (a[min]>a[j])
-				min = j;
-			tmp = a[i];
-			a[i] = a[min];
-			a[min] = tmp;
-		}
-	}
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d\t", a[i]);
+		wannum(i);
 	}
 	printf("\n");
 	return 0;
 	system("pause");
+
 }
