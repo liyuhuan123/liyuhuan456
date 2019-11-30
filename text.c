aab9cc3ebfe595f77f4dd96b47f52b822860bea3
liyuhuan123 <2502268134@qq.com>
2019-11-30 22:47:29 +0800

    提交代码

--- text.c
+++ text.c
@@ -1,27 +1,51 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
-void wannum(int x)
+int main( )
 {
-	int i = 0;
-	int sum = 0;
-	for (i = 1; i < x; i++)
+	/*int a = 1; int b = 2; int t;
+	t = a;
+	a = b;
+	b = t;
+	printf("a=%d,b=%d\n", a, b);*/
+	/*int arr[10] = { 1, 2, 4, 3, 7, 5, 6, 8, 9, 0 };
+	int i,max;
+	max = arr[0];
+	for (i = 0; i < 10; i++)
 	{
-		if (x%i == 0)
-			sum += i;
-	}
-	if (sum == i)
-		printf("%d\t", x);
-}
-int main()
-{
-	int i = 1;
-	for (i = 1; i <= 1000; i++)
-	{
-		wannum(i);
-	}
-	printf("\n");
-	return 0;
-	system("pause");
+		if (max < arr[i])
+		{
+			max = arr[i];
+		}
 
+	}
+	printf("max=%d\n", max);*/
+	/*int a=2, b=3, c=1, t;
+	if (a < b)
+	{
+		t = a;
+		a = b;
+		b = t;
+	}
+	if (a < c)
+	{
+		t = a;
+		a = c;
+		c = t;
+	}
+	if (b < c)
+	{
+		t = b;
+		b = c;
+		c = t;
+	}
+	printf("%d,%d,%d\n", a, b, c);*/
+	int a = 23, b = 69, i,j;
+	for (i = 1; i <= a&&i <= b; ++i)
+	{
+		if (a%i == 0 && b%i == 0)
+			j = i;
+	}
+	printf("\n最大公约数是:%d\n", j);
+	system("pause");
+	return 0;
 }
